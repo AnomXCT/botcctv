1b8e556be138848235420875125bbb483be04960
AnomXCT <115417271+AnomXCT@users.noreply.github.com>
2022-10-23 02:04:20 +0800

    Add files via upload

---
+++ botcctv.rb
@@ -0,0 +1,102 @@
+
+    system("xdg-open https://youtube.com/channel/UC4uniKlNUSV4wool3fRZm4A");
+def ip()
+    puts "≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈";
+    puts "http://119.2.50.116:90/#view";
+    puts "http://119.2.50.116:84/view/viewer_index.shtml?id=1183";
+    puts "http://202.150.130.137:88/control/userimage.html";
+    puts "http://118.137.102.29:8001/view/viewer_index.shtml?id=12647";
+    puts "http://103.217.216.198:8001/#view";
+    puts "http://103.217.216.197:8001/#view";
+    puts "http://103.52.16.102:82/live/index.html?Language=0";
+    puts "http://202.52.50.183:8001/#view";
+    puts "http://103.217.216.198:8000/#view";
+    puts "http://119.252.169.189:82/mobile.htm";
+    puts "http://119.252.169.189:84/";
+    puts "http://103.119.145.26:8001/live/index.html?Language=0";
+    puts "http://36.91.83.73:8081/";
+    puts "http://119.252.169.189:85/";
+    puts "http://210.128.188.40:80";
+    puts "http://202.237.132.50:80";
+    puts "http://61.207.158.202:6000";1
+    puts "http://61.207.158.202:80";
+    puts "http://114.185.129.202:80";
+    puts "http://133.208.209.47:60001";
+    puts "http://210.230.156.125:8888";
+    puts "http://125.174.94.202:80";
+    puts "http://220.254.72.200:80";
+    puts "http://121.114.75.205:8084";
+    puts "http://180.17.132.62:50001";
+    puts "http://153.161.27.161:8080";
+    puts "http://114.152.150.34:8082";
+    puts "http://114.152.150.34:8081";
+    puts "http://118.243.25.164:80";
+    puts "http://124.110.12.158:80";
+    puts "http://118.110.9.158:80";
+    puts "http://118.110.9.158:83";
+    puts "http://58.1.156.165:80";
+    puts "http://218.219.228.113:8";0
+    puts "http://153.175.209.45:82";
+    puts "http://153.175.209.45:83";
+    puts "http://114.151.128.45:80";
+    puts "http://60.33.83.161:83";
+    puts "http://210.173.7.12:6000";1
+    puts "http://61.115.115.49:8082";
+    puts "http://61.115.115.49:8083";
+    puts "http://61.115.115.49:8081";
+    puts "http://210.153.191.157:8081";
+    puts "http://210.153.191.157:8080";
+    puts "http://221.184.214.157:8001";
+    puts "http://180.26.143.8:50001";
+    puts "http://119.150.51.6:50000";
+    puts "http://211.133.62.7:8080";
+    puts "http://58.93.85.121:50001";
+    puts "http://120.51.189.191:8080";
+    puts "http://103.245.19.243/live/index.html?Language=0";
+    puts "http://103.4.206.135:83/ui3.htm";
+    puts "http://119.2.50.114:88/view/viewer_index.shtml?id=3941";
+    puts "http://65.201.171.70/#view";
+    puts "http://205.189.36.99:8082/control/userimage.html";
+    puts "http://75.183.181.155/view/viewer_index.shtml?id=200";
+    puts "http://12.34.170.21:8888/view/viewer_index.shtml?id=639";
+    puts "http://128.104.206.15/view/viewer_index.shtml?id=131";
+    puts "http://24.20.101.215:8000/view/viewer_index.shtml?id=44";
+    puts "http://166.168.156.178:81/live/index.html?Language=0";
+    puts "http://166.153.247.80:82/cgi-bin/guestimage.html";
+    puts "http://75.112.32.101/view/viewer_index.shtml?id=1185";
+    puts "http://166.247.40.1:82/#view";
+    puts "http://67.204.149.29/control/userimage.html";
+    puts "http://74.51.217.90:82/view/viewer_index.shtml?id=1968";
+    puts "http://96.237.61.111:81/live/index.html?Language=0";
+    puts "http://50.248.29.158/view/viewer_index.shtml?id=1315";
+    puts "http://152.26.8.85/view/index.shtml";
+    puts "http://202.73.139.110:81/live/index.html?Language=1&ViewMode=pull";
+    puts "http://58.93.199.162:50001/live/index.html?Language=1";
+    puts "≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈";
+end
+
+def menu()
+    system("clear");
+    system("figlet MR.ANOMXCT ");
+    puts "\e[91m╔════════════════════════════════════════════════╗"                                             ;
+    puts "\e[91m║  \e[91m《•》 AUTHOR   : Mr.AM                       \e[91m ║"
+    puts "\e[91m║  \e[91m《•》 MODIFIER : AnomXCT                     \e[91m ║"
+    puts "\e[91m║  \e[91m《•》 TEAM     : Termux Cyber Team           \e[91m ║"
+    puts "\e[91m║  \e[91m《•》 GITHUB   : https:github.com/AnomXCT    \e[91m ║"
+    puts "\e[91m║  \e[91m《•》 YOUTUBE  : Rangimu channel             \e[91m ║"
+    puts "\e[91m║  \e[91m《•》 FACEBOOK : Anomx Usertermux            \e[91m ║"
+    puts "\e[91m║  \e[91m《•》 INSTAGRAM: @anomx36                    \e[91m ║"
+    puts "\e[91m╚════════════════════════════════════════════════╝"
+    puts ""
+    puts "\e[92m╔═══════════════════════════╗"
+    puts "\e[92m║\e[92m WATCH CCTV            \e[92m    ║"
+    puts "\e[92m╚═══════════════════════════╝"
+    puts "Ip Adreass";
+    ip
+    print "MASUKAN IPnya DISINI 👉 ";
+    mask = gets.chomp
+    system("xdg-open #{mask}")
+
+end
+menu
+
